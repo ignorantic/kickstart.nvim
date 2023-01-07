997b88bca53c3268fc418d01513f175bdb923356
Andrii Sorokin <ansolaris@gmail.com>
2023-01-07 18:42:48 +0200

    Add: nvim-tree

---
+++ after/plugin/defaults.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<C-t>", ":NvimTreeToggle<CR>", {})
+vim.keymap.set("n", "<leader>t", ":NvimTreeFocus<CR>", {})
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,36 @@
+return function(use)
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+
+  -- disable netrw at the very start of your init.lua (strongly advised)
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+
+  -- set termguicolors to enable highlight groups
+  vim.opt.termguicolors = true
+
+  -- OR setup with some options
+  require("nvim-tree").setup({
+    sort_by = "case_sensitive",
+    view = {
+      adaptive_size = true,
+      mappings = {
+        list = {
+          { key = "u", action = "dir_up" },
+          { key = "<C-t>", action = "" },
+        },
+      },
+    },
+    renderer = {
+      group_empty = true,
+    },
+    filters = {
+      dotfiles = true,
+    },
+  })
+end
