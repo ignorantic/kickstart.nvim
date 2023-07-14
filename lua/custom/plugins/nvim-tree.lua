a4b7d2635ed0dc6e21042679ba1441d1dfc4ebf8
Andrii Sorokin <ansolaris@gmail.com>
2023-07-14 19:22:46 +0300

    Add: tree styling

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -13,7 +13,7 @@ return {
         require('nvim-tree').setup({
             sort_by = 'case_sensitive',
             view = {
-                width = 30,
+                width = 25,
             },
             renderer = {
                 group_empty = true,
