f26b7bf59d2317087d7e33bec5142f0f4797bdf1
Andrii Sorokin <ansolaris@gmail.com>
2023-11-27 12:50:32 +0200

    Fix: indend-blankline config

--- lua/custom/plugins/indent-blankline.lua
+++ lua/custom/plugins/indent-blankline.lua
@@ -1,18 +1,24 @@
 return {
-    -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-        char = '',
-        show_trailing_blankline_indent = false,
-        char_highlight_list = {
-            'IndentBlanklineIndent1',
-            'IndentBlanklineIndent2',
-            'IndentBlanklineIndent3',
-            'IndentBlanklineIndent4',
-            'IndentBlanklineIndent5',
-            'IndentBlanklineIndent6',
-        },
-    },
+    main = 'ibl',
+    config = function()
+        require("ibl").setup {
+            indent = {
+                char = "",
+                highlight = {
+                    'IndentBlanklineIndent1',
+                    'IndentBlanklineIndent2',
+                    'IndentBlanklineIndent3',
+                    'IndentBlanklineIndent4',
+                    'IndentBlanklineIndent5',
+                    'IndentBlanklineIndent6',
+                },
+            },
+            whitespace = {
+                highlight = highlight,
+                remove_blankline_trail = false,
+            },
+            scope = { enabled = false },
+        }
+    end,
 }
